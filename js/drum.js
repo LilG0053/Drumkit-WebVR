043a2388014d9e574b7424eb33b5163eb712f4af
LilG0053 <ekimmel8@gatech.edu>
2024-07-12 11:56:27 -0400

    drum sounds working
    -added a working drum sounds scrip
    -modular (serialized sound path)

--- js/drum.js
+++ js/drum.js
@@ -1,4 +1,5 @@
-import {Component, Property} from '@wonderlandengine/api';
+import { Component, Property, Type } from '@wonderlandengine/api';
+import { HowlerAudioSource } from '@wonderlandengine/components';
 
 /**
  * drum
@@ -7,14 +8,24 @@ export class Drum extends Component {
     static TypeName = 'drum';
     static Properties = {
         /** Object that has the button's mesh attached */
-
+        drumSoundPath: { type: Type.String, default: 'sfx/snare.mp3' },
     };
+
+    static onRegister(engine) {
+        engine.registerComponent(HowlerAudioSource);
+    }
 
     init() {
         this.collider = this.object.getComponent('collision');
     }
+
     start() {
-        console.log('start() object: ', this.objectId);
+        console.log('onActivate() object: ', this.objectId);
+        console.log('Drumsoundpath is: ', this.drumSoundPath.toString());
+        this.soundHit = this.object.addComponent(HowlerAudioSource, {
+            src: this.drumSoundPath.toString(),
+            spatial: true,
+        });
     }
 
     update(dt) {
@@ -23,6 +34,10 @@ export class Drum extends Component {
         for (const otherCollision of overLaps) {
             const otherObject = otherCollision.object;
             console.log(`Collision with object ${otherObject.objectId}`);
+            //If it has been hit by the drumstick play the sound
+            if (otherObject.name === 'drumstick') {
+                this.soundHit.play();
+            }
         }
     }
 }

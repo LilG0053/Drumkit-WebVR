30473f24fe92f4441ebfeda8c888e744ea6320d4
LilG0053 <ekimmel8@gatech.edu>
2024-07-16 13:08:06 -0400

    kick drum to 'B' button works now

--- js/kickDrumActivation.js
+++ js/kickDrumActivation.js
@@ -6,11 +6,12 @@ import { HowlerAudioSource } from '@wonderlandengine/components';
  */
 export class KickDrumActivation extends Component {
     static TypeName = 'kickDrumActivation';
+    
     /* Properties that are configurable in the editor */
     static Properties = {
 
     };
-
+    continuouslyPressed = false;
     start() {
         console.log('start() with param', this.param);
         this.kickSound = this.object.addComponent(HowlerAudioSource, {
@@ -21,11 +22,15 @@ export class KickDrumActivation extends Component {
 
     update(dt) {
         if (!this.engine.xr) return;
+        
         //Plays kick sound on A button press
         for (let input of this.engine.xr.session.inputSources) {
-            if (input.gamepad) {
-                if (input.gamepad.buttons[4].pressed) {
+            if (input.handedness === 'left' && input.gamepad) {
+                if (input.gamepad.buttons[5].pressed && !this.continuouslyPressed) {
                     this.kickSound.play();
+                    this.continuouslyPressed = true;
+                } else if (!input.gamepad.buttons[5].pressed) {
+                    this.continuouslyPressed = false;
                 }
             }
              

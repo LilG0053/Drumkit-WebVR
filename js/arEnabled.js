26b47ae1ae25c4686794e2b6ab64dbc8117fa8fe
LilG0053 <ekimmel8@gatech.edu>
2024-07-24 11:59:31 -0400

    Fixed AR skybox issue

--- js/arEnabled.js
+++ js/arEnabled.js
@@ -8,25 +8,41 @@ export class ArEnabled extends Component {
     static TypeName = 'arEnabled';
     /* Properties that are configurable in the editor */
     static Properties = {
-        skyBox: Property.object(),
+        skyBoxes: Property.object(),
         floor: Property.object(),
+        imageCycleButton: Property.object(),
+        raiseButtonMesh: Property.object(),
+        buttonLabel: Property.object(),
     };
-
+    skyboxChildren = null;
 
 
     start() {
         const arButton = document.getElementById('ar-button');
+        this.skyboxChildren = this.skyBoxes.getChildren()
         if (arButton) {
             arButton.addEventListener('click', () => {
-                this.skyBox.active = false;
                 this.floor.active = false;
+                this.imageCycleButton.active = false;
+                this.raiseButtonMesh.active = false;
+                this.buttonLabel.active = false;
+                for (let i = 0; i < this.skyboxChildren.length; i++) {
+                    console.log('Disabling: ' + this.skyboxChildren[i]);
+                    this.skyboxChildren[i].active = false;
+                }
             });
         }
         const vrButton = document.getElementById('vr-button');
         if (vrButton) {
             vrButton.addEventListener('click', () => {
-                this.skyBox.active = true;
                 this.floor.active = true;
+                this.imageCycleButton.active = true;
+                this.raiseButtonMesh.active = true;
+                this.buttonLabel.active = true;
+                for (let i = 0; i < this.skyboxChildren.length; i++) {
+                    console.log('Disabling: ' + this.skyboxChildren[i]);
+                    this.skyboxChildren[i].active = true;
+                }
             })
         }
     }
